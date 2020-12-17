f5a29e1a314872634bd1b471ead3eeeb05030bc8
DevGev <leifminfagel@gmail.com>
2020-12-17 21:03:01 +0100

    Serbian translations

---
+++ localization/sr_Cyrl_RS.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/browser.py" line="106"/>
+        <source>Navigation</source>
+        <translation>Навигација</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="157"/>
+        <source>Back (Alt+J)</source>
+        <translation>Назат (Alt+J)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="164"/>
+        <source>Forward (Alt+K)</source>
+        <translation>Напред (Alt+K)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="190"/>
+        <source>Add Tab (Ctrl+H)</source>
+        <translation>Нови Прозор (Ctrl+H)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="240"/>
+        <source>Save Page</source>
+        <translation>Сачувај Страницу</translation>
+    </message>
+</context>
+</TS>
