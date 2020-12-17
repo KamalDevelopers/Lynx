b83f0319e816423d8e415744e0f444518868770f
DevGev <leifminfagel@gmail.com>
2020-12-18 00:33:50 +0100

    main.py confvar fix

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def runbrowser():
     app.setApplicationName(confvar.BROWSER_WINDOW_TITLE)
     
     translator = QTranslator()
-    if not BROWSER_LOCALE:
+    if not confvar.BROWSER_LOCALE:
         confvar.BROWSER_LOCALE = QLocale.system().name()
     print('Localization loaded:', translator.load(confvar.BROWSER_LOCALE + '.qm', '../localization'), confvar.BROWSER_LOCALE)
     app.installTranslator(translator)
@@ -34,12 +34,12 @@ def runbrowser():
     window = MainWindow()
     window.setWindowTitle(confvar.BROWSER_WINDOW_TITLE)
   
-    if os.path.isfile(confvar.BASE_PATH + "lynx/" + BROWSER_STYLESHEET + "-lynx_logo.png"):
-        app.setWindowIcon(QIcon(confvar.BASE_PATH + "lynx/" + BROWSER_STYLESHEET + "-lynx_logo.png")) 
+    if os.path.isfile(confvar.BASE_PATH + "lynx/" + confvar.BROWSER_STYLESHEET + "-lynx_logo.png"):
+        app.setWindowIcon(QIcon(confvar.BASE_PATH + "lynx/" + confvar.BROWSER_STYLESHEET + "-lynx_logo.png")) 
     else: 
         app.setWindowIcon(QIcon('img/lynx_logo.svg'))
 
-    app.setStyleSheet(open(BASE_PATH + "themes/" + BROWSER_STYLESHEET + ".qss").read())
+    app.setStyleSheet(open(BASE_PATH + "themes/" + confvar.BROWSER_STYLESHEET + ".qss").read())
     app.exec_()
 
 if __name__ == "__main__":
