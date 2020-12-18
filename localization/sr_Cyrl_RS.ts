c86772390882f5d0977879cf3fa338eff4e13e7c
DevGev <leifminfagel@gmail.com>
2020-12-18 20:52:07 +0100

    Added both Serbian and Swedish translations

--- localization/sr_Cyrl_RS.ts
+++ localization/sr_Cyrl_RS.ts
@@ -4,34 +4,44 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/browser.py" line="116"/>
+        <location filename="../src/browser.py" line="129"/>
         <source>Navigation</source>
         <translation>Навигација</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="182"/>
+        <location filename="../src/browser.py" line="195"/>
         <source>Back (Alt+J)</source>
         <translation>Назат (Alt+J)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="189"/>
+        <location filename="../src/browser.py" line="202"/>
         <source>Forward (Alt+K)</source>
         <translation>Напред (Alt+K)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="215"/>
+        <location filename="../src/browser.py" line="228"/>
         <source>Add Tab (Ctrl+H)</source>
         <translation>Нови Прозор (Ctrl+H)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="287"/>
+        <location filename="../src/browser.py" line="314"/>
         <source>Save Page</source>
         <translation>Сачувај Страницу</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="221"/>
+        <location filename="../src/browser.py" line="234"/>
         <source>Stealth Mode (Alt+S)</source>
         <translation>Скривени Режим (Alt+S)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="74"/>
+        <source>Disable Javascript (Alt+Ctrl+A)</source>
+        <translation>Искључити Javascript (Alt+Ctrl+A)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="80"/>
+        <source>Enable Javascript (Alt+Ctrl+S)</source>
+        <translation>Укључити Javascript (Alt+Ctrl+S)</translation>
     </message>
 </context>
 </TS>
--- localization/sv_SE.ts
+++ localization/sv_SE.ts
@@ -4,34 +4,44 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/browser.py" line="116"/>
+        <location filename="../src/browser.py" line="129"/>
         <source>Navigation</source>
         <translation>Navigation</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="182"/>
+        <location filename="../src/browser.py" line="195"/>
         <source>Back (Alt+J)</source>
         <translation>Bakåt (Alt+J)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="189"/>
+        <location filename="../src/browser.py" line="202"/>
         <source>Forward (Alt+K)</source>
         <translation>Frammåt+(Alt+K)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="215"/>
+        <location filename="../src/browser.py" line="228"/>
         <source>Add Tab (Ctrl+H)</source>
         <translation>Ny Flik (Ctrl+H)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="287"/>
+        <location filename="../src/browser.py" line="314"/>
         <source>Save Page</source>
         <translation>Spara Sida</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="221"/>
+        <location filename="../src/browser.py" line="234"/>
         <source>Stealth Mode (Alt+S)</source>
         <translation>Dolt Läge (Alt+S)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="74"/>
+        <source>Disable Javascript (Alt+Ctrl+A)</source>
+        <translation>Inaktivera Javascript (Alt+Ctrl+A)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="80"/>
+        <source>Enable Javascript (Alt+Ctrl+S)</source>
+        <translation>Aktivera Javascript (Alt+Ctrl+S)</translation>
     </message>
 </context>
 </TS>
