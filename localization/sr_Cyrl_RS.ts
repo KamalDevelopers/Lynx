fada9ee2dbac0caa53ecb9685418a0d3c7a6157b
Voki "Pro" Toki <45886099+Fridgie@users.noreply.github.com>
2021-03-18 23:02:47 +0100

    Added Serbian translations

--- localization/sr_Cyrl_RS.ts
+++ localization/sr_Cyrl_RS.ts
@@ -4,44 +4,49 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/browser.py" line="129"/>
+        <location filename="../src/browser.py" line="179"/>
         <source>Navigation</source>
         <translation>Навигација</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="195"/>
+        <location filename="../src/browser.py" line="256"/>
         <source>Back (Alt+J)</source>
         <translation>Назат (Alt+J)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="202"/>
+        <location filename="../src/browser.py" line="263"/>
         <source>Forward (Alt+K)</source>
         <translation>Напред (Alt+K)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="228"/>
+        <location filename="../src/browser.py" line="295"/>
         <source>Add Tab (Ctrl+H)</source>
         <translation>Нови Прозор (Ctrl+H)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="314"/>
+        <location filename="../src/browser.py" line="464"/>
         <source>Save Page</source>
         <translation>Сачувај Страницу</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="234"/>
+        <location filename="../src/browser.py" line="302"/>
         <source>Stealth Mode (Alt+S)</source>
         <translation>Скривени Режим (Alt+S)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="74"/>
+        <location filename="../src/browser.py" line="101"/>
         <source>Disable Javascript (Alt+Ctrl+A)</source>
-        <translation>Искључити Javascript (Alt+Ctrl+A)</translation>
+        <translation>Искључи Javascript (Alt+Ctrl+A)</translation>
     </message>
     <message>
-        <location filename="../src/browser.py" line="80"/>
+        <location filename="../src/browser.py" line="107"/>
         <source>Enable Javascript (Alt+Ctrl+S)</source>
-        <translation>Укључити Javascript (Alt+Ctrl+S)</translation>
+        <translation>Укључи Javascript (Alt+Ctrl+S)</translation>
+    </message>
+    <message>
+        <location filename="../src/browser.py" line="114"/>
+        <source>Downloads (Alt+D)</source>
+        <translation>Скинуто (Alt+D)</translation>
     </message>
 </context>
 </TS>
