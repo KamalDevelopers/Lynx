4d44500d01258ae7013740988e4ecc84a66a937f
DevGev <leifminfagel@gmail.com>
2021-06-06 20:26:26 +0200

    Format script fix

--- format.py
+++ format.py
@@ -5,14 +5,14 @@ from os.path import isfile, join
 
 format_paths = ["src/", "src/utils/"]
 format_command = "black --line-length=79 "
-ignore = ["main.py"]
+ignore = ["confvar.py"]
 files = {}
 for format_path in format_paths:
     files[format_path] = [f for f in listdir(format_path) if isfile(join(format_path, f))]
 
 for i, fp in enumerate(list(files.keys())):
     for ff in list(files.values())[i]:
-        if len(sys.argv) is 2:
+        if len(sys.argv) == 2:
             if ff == sys.argv[1]:
                 os.system(format_command + fp + ff)
         elif ff not in ignore and ".py" in ff:
